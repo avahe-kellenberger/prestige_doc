c64519917acb7f0f5ef3e412858c022ad96e3a8f
Avahe Kellenberger <avahe@protonmail.ch>
2018-11-30 20:21:53 -0500

    `prestige_doc.module.Class` objects are now used in `Module` objects.
    Moved `__init__.py` into a `test` directory.

--- prestige_doc/module.py
+++ prestige_doc/module.py
@@ -1,6 +1,33 @@
 from prestige_doc import parser
 
 
+class Class(object):
+
+    """
+    Represents a python class, which allows access to its documentation and functions.
+    """
+
+    def __init__(self, source_code):
+        """
+        Parses the source code of the given class and breaks it down into documentation and functions.
+
+        Parameters
+        ----------
+        source_code: str
+            The raw text of the class.
+        """
+        self.__source_code = source_code
+
+    @property
+    def source_code(self):
+        """
+        Returns
+        -------
+        str: The source code of the class.
+        """
+        return self.__source_code
+
+
 class Module(object):
 
     """
@@ -9,23 +36,30 @@ class Module(object):
 
     def __init__(self, source_code):
         """
-        Parses the source code of the given module and breaks it down into classes, functions, and global variables.
+        Parses the source code of the given module and breaks it down into classes and global variables.
 
         Parameters
         ----------
         source_code: str
             The raw text of the module.
         """
-        self.__classes = parser.find_classes(source_code)
+        self.__source_code = source_code
+        self.__classes = tuple(Class(source) for source in parser.find_classes(source_code).values())
+
+    @property
+    def source_code(self):
+        """
+        Returns
+        -------
+        str: The source code of the module.
+        """
+        return self.__source_code
 
     @property
     def classes(self):
         """
-        TODO:
-
         Returns
         -------
-        tuple:
-            TODO.
+        tuple: The classes inside the module as `prestige_doc.module.Class` objects.
         """
         return self.__classes
--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,10 @@
-from prestige_doc import parser
 import urllib.request
+
+from prestige_doc.module import Module
 
 if __name__ == "__main__":
     url = 'https://raw.githubusercontent.com/avahe-kellenberger/prestige_irc/master/prestige_irc/connection.py'
     text = urllib.request.urlopen(url).read().decode('utf-8')
 
-    classes = parser.find_classes(text)
-    for class_name in classes.keys():
-        print(classes.get(class_name))
-        print('\n\n')
+    test_module = Module(text)
+    print("\n\n".join(c.source_code for c in test_module.classes))
